539e5a2ef49e63cdcf3faf199e4be7c1911cf6e7
Rodrigo Borges <rodrigo@borges.net.br>
2024-08-20 22:21:03 +0200

    basemap

---
+++ data-raw/basemap.R
@@ -0,0 +1,19 @@
+## code to prepare `basemap` dataset goes here
+#library(brazilmaps)
+library(sf)
+library(geobr)
+library(rmapshaper)
+#Mapa municipios
+#basemap <- get_brmap("City")
+# basemap$codmun <- as.numeric(as.character(trunc(basemap$City/10)))
+
+basemap <- geobr::read_municipality(year=2020)|> ms_simplify(keep=0.01,keep_shapes = TRUE)
+basemap$codmun <- as.numeric(as.character(trunc(basemap$code_muni/10)))
+
+
+basemap <- basemap |>st_cast('MULTIPOLYGON')|>st_cast('POLYGON')
+basemap <- st_as_sf(basemap)|>st_transform(4326)
+
+
+
+usethis::use_data(basemap, overwrite = TRUE)
