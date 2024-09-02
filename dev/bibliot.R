30c959386bf581cbc8e35fc3a81eeb0cf779b7ca
Rodrigo Borges <rodrigo@borges.net.br>
2024-09-02 03:14:05 +0200

    bibliometrix+keywords translated to pt-br

--- dev/bibliot.R
+++ dev/bibliot.R
@@ -1,7 +1,7 @@
 library(bibliometrix)
 library(openalexR)
 options(openalexR.mailto="rodrigo@borges.net.br")
-
+#Sys.setenv("CHROMOTE_CHROME"="opt/google/chrome-linux64/chrome")
 query_oa <- 'pndr OR polycentr* OR ("central places" AND network)'
 
 oa_biblio <- oa_fetch(entity="works",
@@ -49,3 +49,99 @@ wosdfr <- bibliometrix::convert2df(
   dbsource = "wos",format="plaintext")
 
 saveRDS(wosdfr,"dadostat/wosbrx.rds")
+
+oabibx <- readRDS("dadostat/oa_biblio_x.rds")
+oabibr <- readRDS("dadostat/oa_biblio.rds")
+oabibx <- openalexR::oa2bibliometrix(oabibr)
+
+oabibxf <- oabibx|>dplyr::filter(!grepl("COMPUTER SCIENCE",ID), !grepl("BIOLOGY",ID), !grepl("MEDICINE",ID),!grepl("GENETICS",ID),!grepl("CHEMISTRY",ID),
+                                 !grepl("ASTRONOMY",ID),!grepl("PHYSICS",ID))
+
+oabibxf <- oabibx|>dplyr::filter(grepl("ECONO",ID) | grepl("SOCI",ID) | grepl("URBAN",ID) |
+                                   grepl("PUBLIC",ID) | grepl("POLIT",ID) |
+                                 grepl("PLAN",ID))
+
+##Arbitrary, for getting when sustained production started
+oabibxf <- oabibxf|>dplyr::filter(PY>1930)
+
+oabibxf <- oabibxf|>dplyr::distinct(across(id_oa),.keep_all=T)
+
+saveRDS(oabibxf,"dadostat/oa_biblio_xf.rds")
+##translating to portuguese all keywords
+work_kw <- tibble::tibble(workid= oabibxf$id_oa, keywordsai=oabibxf$ID)
+
+##flow - serialize,factorize,get unique levels for translation, revert process
+work_kww <- work_kw|>
+  tidyr::separate_wider_delim(keywordsai,delim=";",too_few="align_start",names_sep="_")|>
+  select(workid,paste0("keywordsai_",1:5))|>
+  tidyr::pivot_longer(-1,names_to="kwn",values_to = "keyword")
+
+work_kww <- work_kww|>mutate(across(keyword,forcats::as_factor))
+
+kwlevels <- levels(work_kww$keyword)
+
+kwlevels <- data.frame(kworigs=paste0(kwlevels,collapse=";"))|>
+  tidyr::separate_longer_position(kworigs,width=49995)
+
+kwlevels <- kwlevels|>mutate(across(kworigs,\(x){gsub(";","; ",x)}))
+
+kwlevels[4,] <- substr(kwlevels$kworigs[1],49996,nchar(kwlevels$kworigs[1]))
+
+#kwlevels[6,] <- substr(kwlevels$kworigs[3],49981,nchar(kwlevels$kworigs[3]))
+
+kwlevels$kworigs[1] <- substr(kwlevels$kworigs[1],1,49994)
+
+kwlevels$kworigs[2] <- substr(kwlevels$kworigs[2],1,49989)
+
+
+googlesheets4::gs4_auth()
+
+langdest <- "pt-BR"
+
+coltrad <- \(x,db=base) {
+  b <- db|>
+    transmute(a =
+                paste0('=googletranslate(A:A;"en";"',x,'")'))
+  renom <- paste0("label_",x)
+  b <- rename_with(b,~ gsub("a",renom,.x),starts_with("a"))
+}
+#2) Envia para a tradução automática do Google
+kwlevels <- bind_cols(kwlevels,coltrad(langdest,kwlevels))
+
+kwlevels <- kwlevels|>mutate(across(contains("label_"),googlesheets4::gs4_formula))
+
+
+googlesheets4::gs4_create(paste0(format.Date(Sys.Date(),"%Y%m%d"),"-bibliometrixpndr"),sheets = kwlevels)
+
+trad <- googlesheets4::gs4_get(
+  googlesheets4::gs4_find(
+    paste0(format.Date(Sys.Date(),"%Y%m%d"),"-bibliometrixpndr"))) |>
+  googlesheets4::read_sheet()
+
+nkwlevels <- data.frame(kwtrad =paste0(toupper(trad$`label_pt-BR`[c(1,4,2,5,3)]),collapse=""))
+
+nkwlevels <- nkwlevels|>
+  tidyr::separate_longer_delim(kwtrad,delim=";")
+
+nkwlevels[372,1] <- "Preço de Oferta(Vendedor)"
+
+nkwlevels$kwtrad <- stringr::str_trim(nkwlevels$kwtrad)
+
+subforcatslevels <- levels(work_kww$keyword)
+names(subforcatslevels) <- nkwlevels$kwtrad
+
+work_kww <- work_kww|>
+  mutate(across(keyword,\(x){as.character(forcats::fct_recode(x,!!!subforcatslevels))}))
+
+fwork_kw <- work_kww|>tidyr::pivot_wider(names_from=kwn,values_from=keyword)
+fwork_kw <- fwork_kw|>tidyr::unite(col="keywordsai",keywordsai_1:keywordsai_5,sep=";")
+
+#check if equal
+
+if (nrow(fwork_kw)==sum(fwork_kw$workid==oabibxf$id_oa)) {
+  oabibxf$ID <- fwork_kw$keywordsai
+}
+
+saveRDS(oabibxf,"dadostat/oabxfpt.rds")
+#workid= oabibxf$id_oa, keywordsai=oabibxf$ID)
+
